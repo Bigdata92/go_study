a677bda1788badc43a8b367dcf8bc7e3e83793c9
JiseopSong <46867559+Bigdata92@users.noreply.github.com>
2022-11-10 23:30:04 +0900

    숫자형 연산

---
+++ src/section5/number_operation3.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// ex1(overflow error : 범위 초과)
+	// var n1 uint8 = math.MaxUint8 + 1
+	// var n2 uint16 = math.MaxUint16 + 1
+	// var n3 uint32 = math.MaxUint32 + 1
+	// var n4 uint64 = math.MaxUint64 + 1
+
+	// ex2(overflow error : 범위 미만)
+	// var n1 uint8 = math.MaxUint8 - 1
+	// var n2 uint16 = math.MaxUint16 - 1
+	// var n3 uint32 = math.MaxUint32 - 1
+	// var n4 uint64 = math.MaxUint64 - 1
+
+}
